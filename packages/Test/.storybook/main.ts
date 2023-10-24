e55b4e875c9f5128ae31497922fa7f1ec55a9d5e
Seojunhwan <seojunhwan@kakao.com>
2023-10-24 23:26:13 +0900

    feat: install storybook into test component

---
+++ packages/Test/.storybook/main.ts
@@ -0,0 +1,39 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+import { join, dirname } from "path";
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  babel: async (options) => {
+    return {
+      ...options,
+      rootMode: "upward",
+      presets: [
+        ...(options.presets || []),
+        [
+          "@babel/preset-react",
+          {
+            runtime: "automatic",
+          },
+          "preset-react-jsx-transform",
+        ],
+      ],
+    };
+  },
+};
+export default config;
