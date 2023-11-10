16cae16f472a45d6fcf498e77cfe44de4b3724e8
sjoleee <cent7425@gmail.com>
2023-11-11 01:16:05 +0900

    chore: showcase remake

---
+++ showcase/next/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+// import { dirname } from "path";
+
+// const packagePath = require.resolve("");
+// const packageDir = dirname(packagePath);
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // `${packageDir}/**/*.{js,ts,jsx,tsx}`,
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
